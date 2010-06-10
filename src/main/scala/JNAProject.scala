41caa4a1582c2f7e8cdb1461ed12c0512679ebab
Sumit Raja <sumitraja@gmail.com>
2010-06-10 23:25:27 +0100

    Project structure complete. Attempting to add dependencies

---
+++ src/main/scala/JNAProject.scala
@@ -0,0 +1,13 @@
+import sbt.{ProjectInfo, DefaultProject, ParentProject}
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: sumitraja
+ * Date: Jun 10, 2010
+ * Time: 9:44:17 PM
+ */
+
+class JNAProject(info: ProjectInfo) extends DefaultProject(info) {
+
+  
+}
