528ec772ffa4cc90272020794098f28e313ca41d
Sumit Raja <sumitraja@gmail.com>
2010-06-10 23:28:30 +0100

    Project structure complete. Attempting to add dependencies

--- project/build/JNAPluginProject.scala
+++ project/build/JNAPluginProject.scala
@@ -1,5 +1,10 @@
 import sbt._
 class JNAPluginProject(info: ProjectInfo) extends PluginProject(info) with IdeaPlugin {
-  override def managedStyle = ManagedStyle.Maven
-  lazy val publishTo = Resolver.file("GitHub Pages", new java.io.File("../sumo.github.com/maven/")) 
+  lazy val publishTo = Resolver.file("GitHub", new java.io.File("../sumo.github.com/maven/"))
+
+  val jnaeratorRepo = "JNAerator Maven Repository" at "http://jnaerator.sourceforge.net/maven"
+  val nativeLibsRepo = "NativeLibs4Java" at "http://nativelibs4java.sourceforge.net/maven"
+  val javaDotNetRepo = "Java.net Repository for Maven" at "http://download.java.net/maven/2"
+
+  val jnaerator = "com.jnaerator" % "jnaerator" % "0.9.3"
 }
