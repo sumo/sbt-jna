64153f5fb4ab493b59a6336992bb6e39d743c9fa
Sumit Raja <sumitraja@gmail.com>
2010-08-26 23:50:02 +0100

    Version to test complete

--- project/build/JNAPluginProject.scala
+++ project/build/JNAPluginProject.scala
@@ -1,10 +1,5 @@
 import sbt._
-class JNAPluginProject(info: ProjectInfo) extends PluginProject(info) with IdeaPlugin {
-  lazy val publishTo = Resolver.file("GitHub", new java.io.File("../sumo.github.com/maven/"))
-
-  val jnaeratorRepo = "JNAerator Maven Repository" at "http://jnaerator.sourceforge.net/maven"
-  val nativeLibsRepo = "NativeLibs4Java" at "http://nativelibs4java.sourceforge.net/maven"
-  val javaDotNetRepo = "Java.net Repository for Maven" at "http://download.java.net/maven/2"
-
-  val jnaerator = "com.jnaerator" % "jnaerator" % "0.9.3"
+import de.element34.sbteclipsify._
+class JNAPluginProject(info: ProjectInfo) extends PluginProject(info) with Eclipsify {
+  val jnaerator = "com.jnaerator" % "jnaerator" % "0.9.4"
 }
--- src/main/scala/JNAProject.scala
+++ src/main/scala/JNAProject.scala
@@ -7,7 +7,12 @@ import sbt.{ProjectInfo, DefaultProject, ParentProject}
  * Time: 9:44:17 PM
  */
 
+
 class JNAProject(info: ProjectInfo) extends DefaultProject(info) {
 
-  
+	val javaOutputDir = "src/main/java"
+	val scalaOutputDir = "src/main/scala"
+	val headerDir = "src/main/headers"
+	val args = List("-noComp", "-o", javaOutputDir, "-scalaOut", scalaOutputDir, headerDir)
+	com.ochafik.lang.jnaerator.JNAerator.main(args.toArray);
 }
