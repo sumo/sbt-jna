462b61743ecc8789f6a7eb4a37d7c6af29f4bd86
Sumit Raja <sumitraja@gmail.com>
2010-08-27 00:15:00 +0100

    Updates to Eclipsify

--- project/build/JNAPluginProject.scala
+++ project/build/JNAPluginProject.scala
@@ -2,4 +2,6 @@ import sbt._
 import de.element34.sbteclipsify._
 class JNAPluginProject(info: ProjectInfo) extends PluginProject(info) with Eclipsify {
   val jnaerator = "com.jnaerator" % "jnaerator" % "0.9.4"
+  override lazy val includePlugin = propertyOptional[Boolean](true)
+  override lazy val includeProject = propertyOptional[Boolean](true)
 }
