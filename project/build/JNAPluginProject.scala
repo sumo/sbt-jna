57d484a6f11a6a93f3fca5598baaf59adab9a5bc
sumitraja <sumit.raja@mediabag.biz>
2011-02-12 19:02:51 +0000

    Updated to JNAerator 0.9.6

--- project/build/JNAPluginProject.scala
+++ project/build/JNAPluginProject.scala
@@ -8,7 +8,7 @@ class JNAPluginProject(info: ProjectInfo) extends PluginProject(info) with Eclip
   
   override def compileOrder = CompileOrder.JavaThenScala
   
-  val jnaerator = "com.jnaerator" % "jnaerator" % "0.9.5"
+  val jnaerator = "com.jnaerator" % "jnaerator" % "0.9.6"
   val junit = "junit" % "junit" % "4.8.1" % "test"
   val scalatest = "org.scalatest" % "scalatest" % "1.1" % "test"
   
